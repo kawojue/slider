9e38b2d24d7796e95ec8b5684ea948cbde2f11cb
Raheem Kawojue <kawojue08@gmail.com>
2022-11-09 11:07:01 +0100

    Setup tailwindcss

---
+++ tailwind.config.cjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.jsx'],
+  theme: {
+    extend: {
+      fontFamily: {
+        "poppins": ['Poppins', 'sans-serif']
+      },
+      colors: {
+        "pry-clr-0": 'rgb(241, 245, 248)',
+        "pry-clr-1": 'rgb(50, 77, 103)',
+        "pry-clr-2": 'rgb(186, 93, 44)',
+        "pry-clr-3": 'rgb(97, 125, 152)',
+        "pry-clr-4": 'rgb(16, 42, 66)',
+      }
+    },
+  },
+  plugins: [],
+}
