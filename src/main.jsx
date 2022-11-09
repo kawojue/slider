2057dc28b0a52cbc852482d01cf4eb5c7a05548b
Raheem Kawojue <kawojue08@gmail.com>
2022-11-09 13:50:00 +0100

    added context

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import './index.css'
 import App from './App'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { DataContext } from './components/Context'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <DataContext>
+      <App />
+    </DataContext>
   </React.StrictMode>
 )
