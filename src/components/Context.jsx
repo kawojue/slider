015d42311efc74de81ffe62edac1b30b369bed80
Raheem Kawojue <kawojue08@gmail.com>
2022-11-09 13:48:52 +0100

    Context - Prev, Next, auto slide

---
+++ src/components/Context.jsx
@@ -0,0 +1,47 @@
+import data from '../data'
+import { createContext, useState, useEffect } from 'react'
+
+const Context = createContext({})
+
+export const DataContext = ({ children }) => {
+    const [active, setActive] = useState(0)
+    const [people, setPoeple] = useState(data[active])
+
+    const prev = () => {
+        if (active === 0) {
+            setActive(data.length - 1)
+        } else {
+            setActive(active - 1)
+        }
+        setPoeple(data[active])
+    }
+
+    const next = () => {
+        if (active === data.length - 1) {
+            setActive(0)
+        } else {
+            setActive(active + 1)
+        }
+        setPoeple(data[active])
+    }
+
+    useEffect(() => {
+        let auto = setInterval(() => {
+            next()
+        }, 2000)
+        return () => {
+            clearInterval(auto)
+        }
+    }, [active])
+
+    return (
+        <Context.Provider value={{
+            people, active, setActive,
+            next, prev
+        }}>
+            {children}
+        </Context.Provider>
+    )
+}
+
+export default Context
