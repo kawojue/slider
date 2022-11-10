c090d9d3251f14125af83a75c7a45e5c1a4c6e4e
Raheem Kawojue <kawojue08@gmail.com>
2022-11-10 12:48:11 +0100

    Edit

--- src/data.js
+++ src/data.js
@@ -1,26 +1,34 @@
-const data = [{
+const data = [
+    {
         id: 1,
+        image: '../img/me.jpg',
+        name: 'kawojue raheem',
+        title: 'software engineer',
+        quote: `I'm an experienced, impact driven developer. I've been learning to code for 3years. I've more experiences in the Frontend and Backend Web Development and really good at problem solving.`,
+    },
+    {
+        id: 2,
         image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
         name: 'maria ferguson',
         title: 'office manager',
         quote: 'Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid viral kombucha gentrify fanny pack raclette pok pok mustache.',
     },
     {
-        id: 2,
+        id: 3,
         image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
         name: 'john doe',
         title: 'regular guy',
         quote: 'Gastropub sustainable tousled prism occupy. Viral XOXO roof party brunch actually, chambray listicle microdosing put a bird on it paleo subway tile squid umami.',
     },
     {
-        id: 3,
+        id: 4,
         image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959121/person-1_aufeoq.jpg',
         name: 'peter smith',
         title: 'product designer',
         quote: 'Drinking vinegar polaroid street art echo park, actually semiotics next level butcher master cleanse hammock flexitarian ethical paleo.',
     },
     {
-        id: 4,
+        id: 5,
         image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
         name: 'susan andersen',
         title: 'the boss',
